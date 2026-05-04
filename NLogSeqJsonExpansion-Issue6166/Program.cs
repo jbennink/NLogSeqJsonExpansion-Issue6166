d0604b6c2494ccb54751f4b2431f36f6029efa1a
Johan Bennink <j.bennink@calway.nl>
2026-05-04 14:54:52 +0200

    Add project files.

---
+++ NLogSeqJsonExpansion-Issue6166/Program.cs
@@ -0,0 +1,32 @@
+﻿using NLog;
+using NLog.Web;
+
+namespace NLogSeqJsonExpansion_Issue6166;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var logger = LogManager.Setup()
+            .LoadConfigurationFromAppSettings()
+            .GetCurrentClassLogger();
+
+        logger.Properties["EXTRA"] = "ExtraValue";
+
+        var o = new SomeObject
+        {
+            Id = 1,
+            Name = "Test"
+        };
+        logger.Log(LogLevel.Info, "SomeObject contents: {@Object}",o);
+
+
+        LogManager.Shutdown(); // Ensure logs are flushed
+    }
+}
+
+internal class SomeObject
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
